477519aeb163a0039e8a5e2e1ebf5e9e9c48db0f
LoisChoji <43037884+LoisChoji@users.noreply.github.com>
2020-02-29 14:58:37 +0100

    added an int:recipe_id to specify the url

--- food_recipes/views.py
+++ food_recipes/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Allrecipes
 from django.template import loader
 
 # Create your views here.
 def Recipes(request):
-    return HttpResponse('this is my view')
-    x = Allrecipes.objects.all()
-    template = loader.get_template('')
-    return HttpResponse('')
+    ar = Allrecipes.objects.all() #ar is just a variable for allrecipes
+    template = loader.get_template('food_recipes/recipes.html')
+
+    context={
+        'ar': ar,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, recipe_id):
-    return HttpResponse("this is the recipe detail page" + str(recipe_id))
+    try:
+        recipe = Allrecipes.objects.get(pk = recipe_id)
+    except Allrecipes.DoesNotExist:
+        raise Http404("Recipe does not exist")
+
+    return render(request, 'food_recipes/detail.html', {'recipe':recipe}))
